7c405d94b6b30d1e9c158cf4d11fc63508a4eded
Goran Ehrsson <goran@technipelago.se>
2015-12-20 12:20:01 +0100

    The documents (files) tab is now added automatically to crmCampaign/show.gsp

--- CrmCampaignUiGrailsPlugin.groovy
+++ CrmCampaignUiGrailsPlugin.groovy
@@ -19,4 +19,23 @@ It contains a Twitter Bootstrap based user interface for campaign management.
     def organization = [name: "Technipelago AB", url: "http://www.technipelago.se/"]
     def issueManagement = [system: "github", url: "https://github.com/technipelago/grails-crm-campaign-ui/issues"]
     def scm = [url: "https://github.com/technipelago/grails-crm-campaign-ui"]
+
+    def doWithApplicationContext = { applicationContext ->
+        def crmPluginService = applicationContext.crmPluginService
+        def crmCoreService = applicationContext.crmCoreService
+        def crmContentService = applicationContext.crmContentService
+        boolean uploadMultipleFiles = application.config.crm.content.upload.multiple ?: false
+
+        if (!crmPluginService.hasView('crmCampaign', 'show', 'tabs', [id: 'documents'])) {
+            // crmCampaign:show << documents
+            crmPluginService.registerView('crmCampaign', 'show', 'tabs',
+                    [id: "documents", index: 500, permission: "crmCampaign:show", label: "crmCampaign.tab.documents.label", template: '/crmContent/embedded', plugin: "crm-content-ui", model: {
+                        def result = crmContentService.findResourcesByReference(crmCampaign)
+                        return [bean     : crmCampaign, list: result, totalCount: result.size(),
+                                multiple: uploadMultipleFiles, status: 'shared',
+                                reference: crmCoreService.getReferenceIdentifier(crmCampaign), openAction: 'open']
+                    }]
+            )
+        }
+    }
 }
