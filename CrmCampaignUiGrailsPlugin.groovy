15b4497385414664a155226d22d0c7a1b0d4b102
Goran Ehrsson <goran@technipelago.se>
2016-08-14 22:48:13 +0200

    Version 2.4.4

--- CrmCampaignUiGrailsPlugin.groovy
+++ CrmCampaignUiGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class CrmCampaignUiGrailsPlugin {
     def groupId = ""
-    def version = "2.4.4-SNAPSHOT"
+    def version = "2.4.4"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCampaign']
