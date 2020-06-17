58547a6f64c2c0437d4f8893de94a9cc3675db45
Goran Ehrsson <goran@technipelago.se>
2020-06-17 16:41:56 +0200

    Version 2.4.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,9 +45,9 @@ grails.project.dependency.resolution = {
         compile ":decorator:1.1"
         compile ":ckeditor:4.5.4.0"
 
-        compile ":crm-campaign:2.4.7-SNAPSHOT"
-        compile ":crm-content-ui:2.4.5"
-        compile ":crm-ui-bootstrap:2.4.3"
+        compile ":crm-campaign:2.4.7"
+        compile ":crm-content-ui:2.5.0"
+        compile ":crm-ui-bootstrap:2.4.4"
 
         compile ":selection:0.9.8"
         runtime ":selection-repository:0.9.3"
